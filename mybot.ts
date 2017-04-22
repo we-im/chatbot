0845af8a5da38af269c030a5fd372e8e301e9c6a
brucx <xty.hello@gmail.com>
2017-04-22 21:34:01 +0800

    添加聊天机器人

--- mybot.ts
+++ mybot.ts
@@ -1,31 +1,43 @@
 import { Wechaty, Room } from 'wechaty'
 import axios from 'axios'
-const bot = Wechaty.instance()
+
+const Robot = require('tuling123-client')
+const { TULING123_API_KEY, SCKEY } = require('./config')
+const tuling = new Robot(TULING123_API_KEY)
+const bot = Wechaty.instance({ profile: 'local' })
+
+let pushMessage = ({ text, desp }) => {
+  console.log('pushMessage:', text, ':', desp)
+  axios.post(`http://sc.ftqq.com/${SCKEY}.send`, { text, desp }).then(data => {
+    console.log(data)
+  }).catch(err => console.error)
+}
 
 bot
   .on('login', user => console.info('Bot', `${user.name()} logined`))
   .on('logout', user => console.info('Bot', `${user.name()} logouted`))
   .on('error', e => {
     console.info('Bot', 'error: %s', e)
-    axios.post('http://sc.ftqq.com/SCU7725T413fb12116bf09f6b5771eb9db36fd4258faeb664e15d.send', {
-      text: '出问题啦',
-      desp: e.toString()
-    })
+
   })
   .on('scan', (url, code) => {
-    let loginUrl = url.replace('qrcode', 'l')
-    require('qrcode-terminal').generate(loginUrl)
-    axios.post('http://sc.ftqq.com/SCU7725T413fb12116bf09f6b5771eb9db36fd4258faeb664e15d.send', {
-      text: '出问题啦',
-      desp: '要重新扫码~'
-    })
-    console.log(url)
+    if (!/201|200/.test(String(code))) {
+      let loginUrl = url.replace('qrcode', 'l')
+      require('qrcode-terminal').generate(loginUrl)
+      pushMessage({
+        text: '扫码请求',
+        desp: '要重新扫码~'
+      })
+      console.log(url)
+    }
   })
 
   .on('friend', async function (contact, request) {
     if (request) {
       await request.accept()
+      console.log('contact: ', contact)
       console.log(`Contact: ${contact.name()} send request ${request.hello}`)
+      contact.say('好开心你来找我啦，回复“进群”让我把你拉进邻居儿的小屋吧!')
     }
   })
 
@@ -34,31 +46,36 @@ bot
     const content = m.content()
     const room = m.room()
 
-    if (m.self()) {
-      return
-    }
+    if (m.self()) return
     if (room) {
       console.log(`Room: ${room.topic()} Contact: ${contact.name()} Content: ${content}`)
-    } else {
-      console.log(`Contact: ${contact.name()} Content: ${content}`)
-      if (/进群/.test(content)) {
-        console.log(`加${contact.name()}进群`)
-        try {
-          let keyroom = await Room.find({ topic: /邻居儿/ })
-          if (keyroom) {
-            await keyroom.add(contact)
-            await keyroom.say("欢迎新同学~", contact)
-          }
-        } catch (e) {
-          console.error(e)
-        }
-
-      }
+      return
     }
-
+    if (/进群/.test(content)) {
+      console.log(`加${contact.name()}进群`)
+      try {
+        let keyroom = await Room.find({ topic: /邻居儿/ })
+        if (keyroom) {
+          await keyroom.add(contact)
+          await keyroom.say("欢迎新同学~", contact)
+        }
+      } catch (e) {
+        console.error(e)
+      }
+      return
+    }
     if (/hello/.test(content)) {
       m.say("hello how are you")
+      return
     }
+    /你已添加了/.test(content) || tuling.ask(m.content(), { userid: m.from() })
+      .then(({ text }) => {
+        console.info('Tuling123', `Talker reply:"${text}" for "${m.content()}" `)
+        m.say(text)
+      })
+      .catch(err => {
+        console.error('Bot', 'on message rejected: %s', err)
+      })
   })
 
   .init()
